fcf7eb4a53cd98af85df4267989cf4ad2d07f36c
Rindone <56407@etu.he2b.be>
2023-05-30 22:30:52 +0200

    changement visuelle

--- js/icon.js
+++ js/icon.js
@@ -7,35 +7,36 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     function incrémenter() {
-        var input = document.querySelector('#input');
-        var oldInput = input.value;
+        var input = document.getElementById('number');
+        let sus  = parseInt(input.textContent);
+        var oldInput = sus;
         if (oldInput < 10) {
-            input.value = parseInt(oldInput) + 1;
+            input.textContent = oldInput + 1;
             newProportions(oldInput);
         }
     }
 
     function décrementer() {
-        var input = document.querySelector('#input');
-        var oldInput = input.value;
+        var input = document.getElementById('number');
+        let sus = parseInt(input.textContent);
+        var oldInput = sus;
         if (oldInput > 1) {
-            input.value = parseInt(oldInput) - 1;
+            input.textContent = oldInput - 1;
             newProportions(oldInput);
         }
     }
 
 
     function newProportions( oldInput) {
-        var input = document.getElementById('input').value; // Récupérer la valeur de l'input
+        var input = document.getElementById('number').textContent; // Récupérer la valeur de l'input
 
-        var spans = document.querySelectorAll('span'); // Sélectionner tous les éléments spans
+        var spans = document.querySelectorAll('span.hasToChange'); // Sélectionner tous les éléments spans
 
         for (var i = 0; i < spans.length; i++) {
             var span = spans[i];
             var inputValue = parseInt(input);//valeur
-            var valeur = parseInt(span.innerHTML); // Récupérer la valeur du span
-
-                span.innerHTML = valeur / oldInput * inputValue; // Calculer la nouvelle proportion
+            var valeur = parseFloat(span.textContent); // Récupérer la valeur du span
+            span.textContent  = ((valeur / oldInput )* inputValue).toFixed(1); // Calculer la nouvelle proportion 
         }
     }
 });
